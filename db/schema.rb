5f466642833edbf314bb90d86d3ba88b176d67b9
Evgeniy-ES <ees86@yandex.ru>
2021-07-21 18:47:26 +0200

    0

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_28_060903) do
+ActiveRecord::Schema.define(version: 2021_07_09_130136) do
 
   create_table "answers", force: :cascade do |t|
     t.string "answer", default: "Good!", null: false
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2021_06_28_060903) do
     t.integer "category_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "level"
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
@@ -52,8 +53,19 @@ ActiveRecord::Schema.define(version: 2021_06_28_060903) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "users_tests", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "test_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["test_id"], name: "index_users_tests_on_test_id"
+    t.index ["user_id"], name: "index_users_tests_on_user_id"
+  end
+
   add_foreign_key "answers", "questions"
   add_foreign_key "categories", "users"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
+  add_foreign_key "users_tests", "tests"
+  add_foreign_key "users_tests", "users"
 end
