d79eb2378c216f12167063626a06195a391d608b
Evgeniy-ES <ees86@yandex.ru>
2021-09-14 22:22:39 +0200

    attempt 1

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,7 +1,7 @@
 class TestsController < ApplicationController
 
   before_action :authenticate_user!
-  before_action :find_test, only: %i[show destroy edit update start]
+  before_action :find_test, only: %i[start]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
 
@@ -9,32 +9,6 @@ class TestsController < ApplicationController
     @tests = Test.all
   end
 
-  def create
-    @test = Test.new(test_params)
-
-    if @test.save
-      redirect_to @test
-    else
-      render :new
-    end
-  end
-
-  def update
-    if @test.update(test_params)
-      redirect_to @test
-    else
-      render :new
-    end
-  end
-
-  def new
-    @test = Test.new
-  end
-
-  def destroy
-    @test.destroy
-    redirect_to tests_path
-  end
 
   def start
     current_user.tests.push(@test)
@@ -45,10 +19,6 @@ class TestsController < ApplicationController
 
   def find_test
     @test = Test.find(params[:id])
-  end
-
-  def test_params
-    params.require(:test).permit(:title, :level, :category_id, :author_id)
   end
 
   def rescue_with_test_not_found
